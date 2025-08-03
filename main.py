468d3bd1f325afd0ff7091c8cfb76c86c6fe5618
damian <damian.temimi@gmail.com>
2025-08-03 22:29:25 +0200

    wip

---
+++ main.py
@@ -0,0 +1,155 @@
+import openmeteo_requests
+from openmeteo_sdk.Variable import Variable
+import requests
+import os
+from dotenv import load_dotenv
+from datetime import datetime, time
+from google import genai
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+receiver = os.getenv("RECEIVER")
+ntfy_channel = os.getenv("NTFY_CHANNEL")
+
+latitude = os.getenv("LATITUDE")
+longitude = os.getenv("LONGITUDE")
+greeting = (
+    f"Napisz wiadomość przeznaczoną dla {receiver} na podstawie poniższej prognozy pogody. "
+    "Opisz jaka jest obecnie pogoda."
+    f"Skoncentruj się na tym, jak {receiver} powinna/powinien się ubrać. "
+)
+
+current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+om = openmeteo_requests.Client()
+params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "current": [
+        "temperature_2m",
+        "relative_humidity_2m",
+        "precipitation",
+        "wind_speed_10m",
+        "cloud_cover",
+        "apparent_temperature",
+        "precipitation_probability",
+    ],
+    "timezone": os.getenv("TIMEZONE"),
+}
+
+def get_weather(parameters):
+    responses = om.weather_api(
+        "https://api.open-meteo.com/v1/forecast", params=parameters
+    )
+    response = responses[0]
+
+
+    # Current values
+    current = response.Current()
+    current_variables = list(
+        map(lambda i: current.Variables(i), range(0, current.VariablesLength()))
+    )
+    current_temperature_2m = next(
+        filter(
+            lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2,
+            current_variables,
+        )
+    )
+    current_relative_humidity_2m = next(
+        filter(
+            lambda x: x.Variable() == Variable.relative_humidity and x.Altitude() == 2,
+            current_variables,
+        )
+    )
+
+    current_precipitation = next(
+        filter(
+            lambda x: x.Variable() == Variable.precipitation,
+            current_variables,
+        )
+    )
+
+    current_wind_speed_10m = next(
+        filter(
+            lambda x: x.Variable() == Variable.wind_speed and x.Altitude() == 10,
+            current_variables,
+        )
+    )
+
+    current_cloud_cover = next(
+        filter(
+            lambda x: x.Variable() == Variable.cloud_cover,
+            current_variables,
+        )
+    )
+
+    current_apparent_temperature = next(
+        filter(
+            lambda x: x.Variable() == Variable.apparent_temperature,
+            current_variables,
+        )
+    )
+
+    current_precipitation_probability = next(
+        filter(
+            lambda x: x.Variable() == Variable.precipitation_probability,
+            current_variables,
+        )
+    )
+
+    forecast_dict = {
+        "temperature": round(current_temperature_2m.Value(), 2),
+        "humidity": current_relative_humidity_2m.Value(),
+        "precipitation": current_precipitation.Value(),
+        "wind_speed": round(current_wind_speed_10m.Value(), 2),
+        "cloud_cover": round(current_cloud_cover.Value(), 2),
+        "apparent_temperature": round(current_apparent_temperature.Value(), 2),
+        "precipitation_probability": round(
+            current_precipitation_probability.Value(), 2
+        ),
+    }
+
+    return forecast_dict
+
+
+def describe_forecast():
+    client = genai.Client(api_key=gemini_api_key)
+    forecast = get_weather(params)
+    prompt = forecast_to_text(forecast)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    return response.text
+
+
+def forecast_to_text(forecast):
+    text = f"{current_datetime}\n{greeting}\nAktualna temperatura wynosi {forecast['temperature']}°C"
+
+    if forecast["apparent_temperature"] != forecast["temperature"]:
+        text += f", ale temperatura odczuwalna to {forecast['apparent_temperature']}°C"
+
+    text += f". Wilgotność wynosi {forecast['humidity']}%, "
+    text += f"zachmurzenie {forecast['cloud_cover']}%, "
+    text += f"wiatr wieje z prędkością {forecast['wind_speed']} km/h"
+
+    if forecast["precipitation_probability"] > 50:
+        text += f", a prawdopodobieństwo opadów to {forecast['precipitation_probability']}% — możliwy deszcz"
+    else:
+        text += f", prawdopodobieństwo opadów jest niewielkie ({forecast['precipitation_probability']}%)"
+
+    if forecast["precipitation"] > 0:
+        text += f". Obecnie pada — opad: {forecast['precipitation']} mm"
+
+    text += "."
+
+    return text
+
+
+def send_ntfy(prompt):
+    requests.post(
+        ntfy_channel,
+        data=prompt.encode(encoding="utf-8"),
+    )
+
+
+if __name__ == "__main__":
+    send_ntfy(describe_forecast())
